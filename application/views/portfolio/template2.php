f3876809395d7998864b2d9f7442162baaf40ec0
usamafarooq <usama.farooq91@outlook.com>
2018-05-05 12:08:42 +0500

    template

---
+++ application/views/portfolio/template2.php
@@ -0,0 +1,12 @@
+
+<section class="wow fadeIn one-fourth-screen  full-screen cover-background" style="background-image: url(<?php echo base_url($portfolio['Image']) ?>)">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12"></div>
+                </div>
+            </div>
+        </section>
+    <!-- end about product section -->
+    
+
+    <?php echo $portfolio['html'] ?>
